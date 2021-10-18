9fd2ded8ba32d777552cecd75039ba169ecbd30a
initiativist <moorecaleb00@gmail.com>
2021-10-18 11:55:46 -0600

    lazy loading auth code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,3 @@
-// Routing module for the app (from home)
-
 //Angular Imports
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -7,8 +5,6 @@ import { RouterModule, Routes } from '@angular/router';
 // Component Imports
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
@@ -18,9 +14,11 @@ const routes: Routes = [
     path: 'edit/:postId',
     component: PostCreateComponent,
     canActivate: [AuthGuard],
-  }, // Edit Post (handled by newpost component)
-  { path: 'login', component: LoginComponent }, // Login
-  { path: 'signup', component: SignupComponent }, // signup
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 // Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './auth/login/login.component';
 
 // Services and Routing
 import { PostsService } from './posts/posts.service';
 import { AppRoutingModule } from './app-routing.module';
-import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 import { errorComponent } from './error/error.component';
@@ -21,18 +18,11 @@ import { AngularMaterialModule } from './angular-material.module';
 import { PostsModule } from './posts/posts.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    LoginComponent,
-    SignupComponent,
-    errorComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, errorComponent],
   imports: [
     // Angular Core Imports
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
     AngularMaterialModule,
